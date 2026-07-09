f3c36815b94e76ce28d138dcf945e78a22707ee0
artxficial <127349556+artxficial@users.noreply.github.com>
2026-07-09 00:55:13 -0400

    Update esp_utility.lua

--- esp_utility.lua
+++ esp_utility.lua
@@ -272,7 +272,7 @@ function ESP_Utility:_Update()
 
 	-- Update texts
 	for _, TextReference in self.DrawingOrder do 
-		if not self.Name then return end 
+		if not self._Position then return end 
 		local Data = self.Drawings[TextReference]
 		local DrawingObject = Data.Drawing
 		local Callback = Data.Function
